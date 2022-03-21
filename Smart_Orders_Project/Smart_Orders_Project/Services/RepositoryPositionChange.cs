47ea3f18f356b867d78ee88427e525132c759996
Christos Kaktsis <christoskaktsis@gmail.com>
2022-03-21 13:22:17 +0200

    restofposition done

--- Smart_Orders_Project/Smart_Orders_Project/Services/RepositoryPositionChange.cs
+++ Smart_Orders_Project/Smart_Orders_Project/Services/RepositoryPositionChange.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Smart_Orders_Project.Services
 {
-    public class RepositoryPositionChange: RepositoryService
+    public class RepositoryPositionChange : RepositoryService
     {
         int result = 0;
         public async Task<bool> PositionChange(Position position, Product product, int quantity, int impexp)
@@ -18,7 +18,7 @@ namespace Smart_Orders_Project.Services
             var barcode = string.IsNullOrEmpty(product.BarCode) ? "null" : product.BarCode;
 
             StringBuilder sb = new StringBuilder();
-            sb.AppendFormat(await GetParamAsync("postPosition"),Oid, position.Oid, barcode, product.Oid, quantity, impexp);
+            sb.AppendFormat(await GetParamAsync("postPosition"), Oid, position.Oid, barcode, product.Oid, quantity, impexp);
             string queryString = sb.ToString();
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
@@ -27,6 +27,40 @@ namespace Smart_Orders_Project.Services
                 result = command.ExecuteNonQuery();
             }
             return await Task.FromResult(result != 0);
+        }
+        public async Task<List<Product>> GetProductsFromList(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return null;
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.AppendFormat(await GetParamAsync("getProductWithName"), id);
+            //string queryString = sb.ToString();
+            string queryString = $@"select * from (SELECT Είδος, BarCodeΕίδους, sum( IIF([ΤύποςΚίνησηςΘέσης]=1,[ΠοσότηταΕγγραφής]*-1,[ΠοσότηταΕγγραφής])) as Ποσότητα
+FROM[maindemo].[dbo].[ΚίνησηΘέσης] where Θέση = '{id}' group by[Είδος], [BarCodeΕίδους] ) a left join
+(SELECT[Είδος].[Oid],[Κωδικός],[Είδος].[Περιγραφή] FROM[maindemo].[dbo].[Είδος]) b on a.Είδος = b.Oid";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                /*ProductsList.Clear()*/
+                List<Product> ProductsList = new List<Product>();
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+                while (reader.Read())
+                {
+                    ProductsList.Add(new Product()
+                    {
+                        Oid = Guid.Parse(reader["Oid"].ToString()),
+                        ProductCode = reader["Κωδικός"] != DBNull.Value ? reader["Κωδικός"].ToString() : string.Empty,
+                        BarCode = reader["BarCodeΕίδους"] != DBNull.Value ? reader["BarCodeΕίδους"].ToString() : string.Empty,
+                        Name = reader["Περιγραφή"].ToString(),
+                        Quantity = int.Parse(reader["Ποσότητα"].ToString())
+                    });
+                }
+                return await Task.FromResult(ProductsList);
+
+            }
         }
     }
     
